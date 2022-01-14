4b3e9b535f25ec1ba316bd0b01eda9b5bad6cffb
Olga Shevchenko <duvasu7777@gmail.com>
2022-01-14 17:40:53 +0200

    updated video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const STORAGE_TIME = "videoplayer-current-time"
+
+const iframe = document.querySelector('iframe');
+const playerEl = new Player(iframe);
+    
+playerEl.on('timeupdate', throttle(function () {
+    playerEl.getCurrentTime().then(function (seconds) {
+        console.log(seconds)
+        localStorage.setItem(STORAGE_TIME, JSON.stringify(seconds))
+    });
+}, 1000)
+);
+
+const savedTime = JSON.parse(localStorage.getItem(STORAGE_TIME)) || 0;
+        console.log(savedTime)
+         playerEl.setCurrentTime(savedTime)
+    
